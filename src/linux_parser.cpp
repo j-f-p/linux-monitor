81866399795cc2247d6e88cf2d5f8c5581e98a7d
JF Padilla <emailreel@gmail.com>
2019-09-12 10:28:37 -0400

    feat: display number of running processes
    feat:
    + Display number of running processes.
    
    docs:
    * Update comments.

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -98,7 +98,7 @@ long LinuxParser::IdleJiffies() { return 0; }
 // TODO: Read and return CPU utilization
 vector<string> LinuxParser::CpuUtilization() { return {}; }
 
-// TODO: Read and return the total number of processes
+// Read and return the total number of processes.
 int LinuxParser::TotalProcesses() {
   string line;
   string key;
@@ -117,8 +117,24 @@ int LinuxParser::TotalProcesses() {
   return -1;
 }
 
-// TODO: Read and return the number of running processes
-int LinuxParser::RunningProcesses() { return 0; }
+// Read and return the number of running processes.
+int LinuxParser::RunningProcesses() {
+  string line;
+  string key;
+  int firstValue;
+  std::ifstream filestream(kProcDirectory + kStatFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> key >> firstValue) {
+        if (key == "procs_running") {
+          return firstValue;
+        }
+      }
+    }
+  }
+  return -1;
+}
 
 // TODO: Read and return the command associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
