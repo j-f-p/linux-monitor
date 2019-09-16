ef09635473694c0095030ec7dfc80f5728d1edf5
JF Padilla <emailreel@gmail.com>
2019-09-16 08:55:57 -0400

    feat: display processor usage per 1 sec intervals

--- include/processor.h
+++ include/processor.h
@@ -1,6 +1,9 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
+#include <chrono>
+#include <thread>
+
 class Processor {
  public:
   float Utilization();
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,17 +1,24 @@
 #include "processor.h"
 #include "linux_parser.h"
 
+using std::chrono::milliseconds;
+using std::this_thread::sleep_for;
 using std::string;
 using std::unordered_map;
 
 // Return the aggregate CPU utilization.
 float Processor::Utilization() {
   unordered_map<string, long> ticks = LinuxParser::aggregateCPUtickData();
-
   long activetickTypes = ticks["user"] + ticks["nice"] + ticks["system"]
     + ticks["irq"] + ticks["softirq"] + ticks["steal"];
-
   long idletickTypes = ticks["idle"] + ticks["iowait"];
+
+  sleep_for(milliseconds(1000));
+
+  ticks = LinuxParser::aggregateCPUtickData();
+  activetickTypes = ticks["user"] + ticks["nice"] + ticks["system"]
+    + ticks["irq"] + ticks["softirq"] + ticks["steal"] - activetickTypes;
+  idletickTypes = ticks["idle"] + ticks["iowait"] - idletickTypes;
 
   return static_cast<float>(activetickTypes)
     / static_cast<float>(activetickTypes + idletickTypes);
